d2d327d71617f65647e724dd750987b26b0c1648
F-Sampaio <f.s1488@outlook.com>
2020-09-27 21:25:14 -0400

    function createElementsforCurrent created

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,17 @@
-import {requestCurrentWeather, requestForecastWeather, switchThermometricScale} from './search';
-let inputCityName = document.getElementById('cityName');
-
+import {switchThermometricScale, searchWeather} from './search';
+export let inputCityName = document.getElementById('cityName');
 
 document.getElementById('searchBtn').addEventListener('click', searchWeather)
-
-export function searchWeather() {
-    requestCurrentWeather(inputCityName.value);
-    requestForecastWeather(inputCityName.value);
-}
-
 document.getElementById('switchThermometricScale').addEventListener('click', switchThermometricScale);
 
+export const createElementsforCurrent = (data) => {
+    let elementCityName = document.createElement("h1");
+    let elementTempMax = document.createElement("p");
 
+    let valueCityName = document.createTextNode(data.name);
+    let valueTempMax = document.createTextNode(data.main.temp_max)
+
+    elementCityName.appendChild(valueCityName);
+    elementTempMax.appendChild(valueTempMax);
+    document.getElementById('response').append(elementCityName, elementTempMax);
+}
