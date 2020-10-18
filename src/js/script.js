f2e21ff6aa60b9604386c6a7fb1ea25eef3ca295
F-Sampaio <f.s1488@outlook.com>
2020-10-17 23:10:45 -0400

    current finished

--- src/js/script.js
+++ src/js/script.js
@@ -5,18 +5,24 @@ document.getElementById('searchBtn').addEventListener('click', searchWeather)
 document.getElementById('switchThermometricScale').addEventListener('click', switchThermometricScale);
 
 export const responseElementsforCurrent = (data) => {
-        document.getElementById('currentCard').style.visibility = 'visible';
-        document.getElementById('response_cityName').innerHTML = data.name;
-        document.getElementById('response_current_temp').innerHTML = `${data.main.temp_max} ºC`;
-        document.getElementById('response_current_weatherIcon').setAttribute("src", getIcon(data));
-        document.getElementById('response_current_weatherDescription').innerHTML = data.weather[0].description;
-    }
+    document.getElementById('currentCard').style.visibility = 'visible';
+    document.getElementById('response_cityName').innerHTML = data.name;
+    document.getElementById('response_current_temp').innerHTML = `${Math.round(data.main.temp)}º`;
+    document.getElementById('response_current_weatherIcon').setAttribute("src", getIcon(data));
+    document.getElementById('response_current_weatherDescription').innerHTML = data.weather[0].description;
+}
     
-    const getIcon = (data) => {
-        let iconCode = data.weather[0].icon;
-        let iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
-        return iconUrl;
-    }
+const getIcon = (data) => {
+    let iconCode = data.weather[0].icon;
+    let iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+    return iconUrl;
+}
+
+
+
+
+
+
 
 
 
--- src/js/search.js
+++ src/js/search.js
@@ -4,17 +4,25 @@ let flag = true;
 
 export const requestCurrentWeather = (cityName) => {
     let URL_CURRENT_WEATHER = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${currentKey}`
-    if(flag) { URL_CURRENT_WEATHER += `&units=metric`;}
+    if(flag) {
+        URL_CURRENT_WEATHER += `&units=metric`;
+    } else {
+        URL_CURRENT_WEATHER += `&units=imperial`
+    }
     fetch(URL_CURRENT_WEATHER, {method:'get'})
         .then(response => { response.json()
-            .then(result => {console.log(result); responseElementsforCurrent(result) });
+            .then(result => {responseElementsforCurrent(result) });
         })
         .catch(error => { console.error(error); });
 }
 
 export const requestForecastWeather = (cityName) => { 
     let URL_FORECAST_WEATHER = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${forecastKey}`
-    if(flag) { URL_FORECAST_WEATHER += `&units=metric`;}
+    if(flag) {
+        URL_FORECAST_WEATHER += `&units=metric`;
+    } else {
+        URL_FORECAST_WEATHER += `&units=imperial`
+    }
     fetch(URL_FORECAST_WEATHER, {method:'get'})
         .then(response => { response.json()
             .then(result => { console.log(result)});
